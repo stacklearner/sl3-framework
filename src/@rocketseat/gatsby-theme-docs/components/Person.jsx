0574f1ef54e726e5bff64ef4d1e48f901a7d2823
HM Nayem <hmnayem@stacklearner.com>
2020-08-26 06:24:20 +0600

    Added Article from Kickstart to Why do we learn programming

---
+++ src/@rocketseat/gatsby-theme-docs/components/Person.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const wrapperStyle = {
+	textAlign: 'center',
+	marginTop: '3rem',
+	marginBottom: '1rem',
+	display: 'inline-block',
+};
+
+const imgWrapperStyle = {
+	width: '300px',
+	height: '300px',
+	borderRadius: '50%',
+	overflow: 'hidden',
+};
+
+const imgStyle = {
+	width: '300px',
+	height: 'auto',
+};
+
+const h3Style = {
+	fontSize: '20px',
+	fontWeight: 300,
+	color: '#424242',
+	marginBottom: '0.8rem',
+	marginTop: '2rem',
+};
+
+const pStyle = {
+	fontSize: '16px',
+	color: '#666666',
+	marginBottom: '2rem',
+	lineHeight: 1,
+};
+
+const Person = ({ name, company, title, img }) => {
+	return (
+		<div style={wrapperStyle}>
+			<div style={imgWrapperStyle}>
+				<img style={imgStyle} src={img} alt={name + ' - ' + title} />
+			</div>
+			<h3 style={h3Style}>{name}</h3>
+			<p style={pStyle}>
+				{title}
+				<br />
+				{company}
+			</p>
+		</div>
+	);
+};
+
+export default Person;
