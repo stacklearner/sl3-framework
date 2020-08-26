c319802a55b0fd3eefec645426645b28b8b39fcb
HM Nayem <hmnayem@stacklearner.com>
2020-08-26 15:43:43 +0600

    Breaking Changes with Welcome Page

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
-
-const Header = styled.h1`
-	color: #424242;
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 28px;
-	font-weight: 500;
-	text-decoration: none;
-`;
-
-const Small = styled.small`
-	font-size: 16px;
-	color: #424242;
-`;
+import LogoImage from './SL3_LOGO.png';
 
 export default () => (
-	<Header>
-		SL3 Framework <br /> <Small>Code For Brain</Small>{' '}
-	</Header>
+	<img
+		src={LogoImage}
+		alt='SL3 Framework - Code For Brain'
+		title='SL3 Framework - Code For Brain'
+	/>
 );
