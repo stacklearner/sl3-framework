1a130faace487441fc2d10a356d6efcbb41fa165
HM Nayem <hmnayem@stacklearner.com>
2020-08-29 07:33:15 +0600

    Added Github URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
 			options: {
 				configPath: `src/config`,
 				docsPath: `src/docs`,
-				githubUrl: `https://github.com/rocketseat/gatsby-themes`,
-				baseDir: `examples/gatsby-theme-docs`,
+				githubUrl: `https://github.com/stacklearner/sl3-framework`,
+				baseDir: `/`,
 			},
 		},
 		{
